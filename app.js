16e1eba18e42afd77ea3c248061ab1f5d75a9588
Fabio <clonefabster7@yahoo.com>
2023-09-01 17:45:24 -0700

    Sorted highest to lowest from an array of objects

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
- //Given a rating, display a star for each full rating and a full-stop for each half rating
-   
- function showRating (rating) {
-let ratings = ""
- for (let i = 0; i < Math.floor(rating); ++i) {
-    ratings += "*"
+//Given a rating, display a star for each full rating and a full-stop for each half rating
+
+function showRating(rating) {
+  let ratings = "";
+  for (let i = 0; i < Math.floor(rating); ++i) {
+    ratings += "*";
     if (i !== Math.floor(rating) - 1) {
-        ratings += " "
+      ratings += " ";
     }
+  }
+  if (!Number.isInteger(rating)) {
+    ratings += " .";
+  }
+
+  return ratings;
 }
-    if (!Number.isInteger(rating)) {
-ratings += " ."
-    }
-
-return ratings
-
-
- }
 console.log(showRating(5));
 console.log(showRating(4));
 console.log(showRating(2.5));
-console.log(showRating(.5));
+console.log(showRating(0.5));
+
+
+//Sort by lowest to highest price
+
+function sortLowToHigh (arr) {
+ return arr.sort((a, b) => a - b);
+}
+
+console.log(sortLowToHigh([20, 40, 10, 30, 50, 10]));
+
+//Sort highest to lowest price from an array of objects
+
+function sortHighToLow (numbers) {
+  return numbers.sort((a, b) => b.price - a.price); 
+}
+
+console.log(
+  sortHighToLow([
+  { id: 1, price: 50 },
+  { id: 3, price: -500 },
+  { id: 2, price: 0 },
+])
+);
+
+console.log(
+  sortHighToLow([
+  { id: 1, price: 50 },
+  { id: 2, price: 8000 },
+  { id: 3, price: -20 },
+  { id: 4, price: 365 },
+])
+);
