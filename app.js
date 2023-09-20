cf07001777d96b811d6c3fcbbbd82a799e8f625f
Fabio <clonefabster7@yahoo.com>
2023-09-20 13:06:59 -0700

    Calling APIs and returning values

--- app.js
+++ app.js
@@ -19,34 +19,64 @@ console.log(showRating(4));
 console.log(showRating(2.5));
 console.log(showRating(0.5));
 
-
 //Sort by lowest to highest price
 
-function sortLowToHigh (arr) {
- return arr.sort((a, b) => a - b);
+function sortLowToHigh(arr) {
+  return arr.sort((a, b) => a - b);
 }
 
 console.log(sortLowToHigh([20, 40, 10, 30, 50, 10]));
 
 //Sort highest to lowest price from an array of objects
 
-function sortHighToLow (numbers) {
-  return numbers.sort((a, b) => b.price - a.price); 
+function sortHighToLow(numbers) {
+  return numbers.sort((a, b) => b.price - a.price);
 }
 
 console.log(
   sortHighToLow([
-  { id: 1, price: 50 },
-  { id: 3, price: -500 },
-  { id: 2, price: 0 },
-])
+    { id: 1, price: 50 },
+    { id: 3, price: -500 },
+    { id: 2, price: 0 },
+  ])
 );
 
 console.log(
   sortHighToLow([
-  { id: 1, price: 50 },
-  { id: 2, price: 8000 },
-  { id: 3, price: -20 },
-  { id: 4, price: 365 },
-])
+    { id: 1, price: 50 },
+    { id: 2, price: 8000 },
+    { id: 3, price: -20 },
+    { id: 4, price: 365 },
+  ])
 );
+
+//Find all the posts by a single user - call API and return all posts by any given user ID
+
+async function postsByUser(userId) {
+  const promise = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const result = await promise.json();
+
+  const posts = result.filter((element) => element.userId === userId);
+
+  console.log(posts);
+}
+
+postsByUser(4);
+postsByUser(6);
+postsByUser(10);
+
+
+
+//Call API and return the first 6 incomplete todo's
+
+async function firstSixIncomplete(userId1) {
+  const promise1 = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const result1 = await promise1.json()
+
+  const incompleteTasks = result1.filter(elem => !elem.completed).slice(0, 6);
+
+  console.log(incompleteTasks);
+}
+
+
+firstSixIncomplete(6);
